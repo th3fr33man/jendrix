16faac447a49b55968c349670b661d658edf7e0c
rom <raczkaromain@gmail.com>
2012-11-30 18:00:49 +0100

    maj fichier arduino

--- Dev/Arduino/sketch_nov29a/sketch_nov29a.ino
+++ Dev/Arduino/sketch_nov29a/sketch_nov29a.ino
@@ -1,5 +1,8 @@
 //#include <Audio.h>
 //#include <DAC.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Audio.h>
 
 /* Premier programme pour vérifier 
 le son capter sur entrée analogique 
@@ -10,7 +13,7 @@ des tensions produites*/
 int led1 = 10;
 int led2 = 12;
 int sensorValue1 = 0;
-//int sensorValue2 = 0;
+int hpPin = DAC0;
 const int sonInPin = A0;
 
 
@@ -19,30 +22,57 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
+  //pinMode(sonInPin, INPUT _ ANALOG)
+
+  Audio.begin(44100, 100);
+  //Serial.begin(9600);
+
+  //pinMode(hpPin, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-//sensorValue1 = analogRead(sonInPin);
-  for (int i=0;i<1024;i++){
-  sensorValue1 = i;
+  
+  analogReadResolution(12); 
+  analogWriteResolution(12);
+  const int S = 4095;
+  short buffer[];
+  int volume = 1024;
+  
+  sensorValue1 = analogRead(sonInPin);
   sonEnCouleur(sensorValue1);
-  delay(10);
-  }
+  
+  //File myFile = sensorValue1;
+  //buffer=sensorValue1;
+  //Serial.print(sensorValue1);
   
   
+  Audio.prepare(buffer, S, volume);
+  Audio.write(buffer, S);
+  
+  //read(buffer, sizeof(buffer));
+  
+  //delay(10);
+  
+  //float voltage = sensorValue1 * (3.3 / 1023.0);
+
+  //analogWrite(12, map(sensorVal, 0, 1023, 0, 4095));
 }
 
 
 
 void sonEnCouleur(int a){
-  if (a<512){
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  }
-  else if (a>512){
+  if (a>1900 && a<2100){
   digitalWrite(led1, LOW);
   digitalWrite(led2, HIGH);
   }
+  else if (a>2100 || a<1900){
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  }
 }
+
+
+
+
   
