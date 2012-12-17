32644dd39f2558af6eaba46f5c88534647d46db2
rom <raczkaromain@gmail.com>
2012-12-17 20:23:52 +0100

    maj sketch arduino utilisant le serial et le spi

--- Dev/Arduino/sketch_test_serial/sketch_test_serial.ino
+++ Dev/Arduino/sketch_test_serial/sketch_test_serial.ino
@@ -1,40 +1,76 @@
-
+//#include <string.h>
  
 typedef unsigned char uint8_t;
 const char *debModCom = "$$$\n";
 const char *finModCom = "exit\n";
-const char *setSsid = "set$wlan$ssid$test2\n"; 
-const char *setIp = "set$ip$a$10.42.0.200\n";
-const char *setMask = "set$ip$netmask$255.255.255.0\n";
-const char *ping = "Ping$10.42.0.1$10\n";
-
+const char *setSsid = "set wlan ssid FreeWifi\n"; 
+const char *setIp = "set ip a 10.42.0.200\n";
+const char *setMask = "set ip netmask 255.255.255.0\n";
+const char *ping = "Ping 10.42.0.1 10\n";
+const char *joinSsid = "join FreeWifi\n";
 
 const char *enter = "\n"; 
 const char *set = "set"; 
+int byteSent;
 
-
-SoftwareSerial WiFly = new SoftwareSerial(0, 1); // RX, TX
+char *buffer;
 
 
 void setup(){
  Serial.begin(9600);
  Serial1.begin(9600);
- Serial2.begin(9600);
+ //Serial2.begin(9600);
+ byteSent=Serial1.write(debModCom);
+// Serial1.readBytes(buffer, strlen(buffer)) ;
+//  Serial.println(buffer,BIN);
+
+ //Serial.println(Serial.find("COM"));
+  Serial.println("mode commande");
+ Serial.println(byteSent);
+ delay(1000);
+ 
+ byteSent=Serial1.write(setSsid);
+ Serial.println("ssid");
+ Serial.println(byteSent);
+ delay(1000);
+ 
+ byteSent=Serial1.write(setIp);
+ Serial.println("ip");
+ Serial.println(byteSent);
+ delay(1000);
+ 
+ byteSent=Serial1.write(setMask);
+ Serial.println("mask");
+ Serial.println(byteSent);
+ Serial1.write(joinSsid);
+ byteSent=Serial1.write(finModCom);
+ delay(1000);
+ 
+ 
+
 }
 
 
 void loop(){      
   
-  int etat = 0;
-  case 0:
-  SendCommand(debModCom);
+//  int etat = 0;
+//  case 0:
+//  SendCommand(debModCom);
+//
+// 
+//  break;
+ 
+
+
 
  
-  break;
  
+ 
+ Serial.println("mode loop");
+ Serial.println(Serial1.read());
+ delay(1000);
+
   
-  WiFly.println(debModCom);
-  
-  Serial.WiFly.read()
+
   
 }
--- Dev/Arduino/sketch_test_spi1/sketch_test_spi1.ino
+++ Dev/Arduino/sketch_test_spi1/sketch_test_spi1.ino
@@ -1,126 +1,56 @@
-#include <string.h>
-//
-//
-//#define THR        0x00 << 3
-//#define RHR        0x00 << 3
-//#define IER        0x01 << 3
-//#define FCR        0x02 << 3
-//#define IIR        0x02 << 3
-//#define LCR        0x03 << 3
-//#define MCR        0x04 << 3
-//#define LSR        0x05 << 3
-//#define MSR        0x06 << 3
-//#define SPR        0x07 << 3
-//#define TXFIFO     0x08 << 3
-//#define RXFIFO     0x09 << 3
-//#define DLAB       0x80 << 3
-//#define IODIR      0x0A << 3
-//#define IOSTATE    0x0B << 3
-//#define IOINTMSK   0x0C << 3
-//#define IOCTRL     0x0E << 3
-//#define EFCR       0x0F << 3
-//////
-//#define DLL        0x00 << 3
-//#define DLM        0x01 << 3
-//#define EFR        0x02 << 3
-//#define XON1       0x04 << 3  
-//#define XON2       0x05 << 3
-//#define XOFF1      0x06 << 3
-//#define XOFF2      0x07 << 3
-//
-////
-////
-//#define CS         SS
-//#define MOSI       11
-//#define MISO       12
-//#define SCK        13
- #include <SoftwareSerial.h>
- //#include <SPI.h>
- typedef unsigned char uint8_t;
+#include <SPI.h>
+typedef unsigned char uint8_t;
 
-//const int cs = 10;
-//const int mosi = 11;
-//const int miso = 12;
-//const int sck = 13;
-//char ch="$";
+int slaveSelectPin = 10;
 const char *debModCom = "$$$\n";
 const char *finModCom = "exit\n";
 const char *setSsid = "set$wlan$ssid$test2\n"; 
 const char *setIp = "set$ip$a$10.42.0.200\n";
 const char *setMask = "set$ip$netmask$255.255.255.0\n";
 const char *ping = "Ping$10.42.0.1$10\n";
-
-
 const char *enter = "\n"; 
 const char *set = "set"; 
+const char *rejoindre = "join$test3\n";
 
-
-//const uint8_t COM = "$$$\n";
-uint8_t *test ;
-SoftwareSerial WiFly(0, 1); // RX, TX
 
 void setup(){
  Serial.begin(9600);
+ SPI.begin(10);
+ pinMode (slaveSelectPin, OUTPUT);
+ 
 }
 
 
 void loop(){
- int etat = 0;
- case 0:
-   SendCommand(debModCom);
-
- 
- break;
- 
-
-
-
-
-void setup()  
-{
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-
-  Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, world?");
+ int static etat = 0;
+   switch(etat){
+   case 0:
+     digitalWrite(slaveSelectPin,LOW);
+     SendCommand(debModCom);
+     delay(1000);
+     
+     SendCommand(setSsid);
+     SendCommand(setIp);
+     SendCommand(setMask);
+     //SendCommand(finModCom);
+     etat++;
+   break;
+   
+   case 1:
+     SendCommand(rejoindre);
+     delay(10000);
+   break;
+   }
 }
-
-void loop() // run over and over
-{
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
-}
- 
- 
- 
- /*SendCommand(setSsid);
- SendCommand(setIp);
- SendCommand(setMask);
- SendCommand(finModCom);
- SendCommand(ping);
- */
-}
-
-
-
 
 
 void SendCommand(const char *c){
   int i = 0;
   for (i=0  ; i < strlen(c); i++){
-  byte response = SPI.transfer(SS,(unsigned char)c[i]);
+  byte response = SPI.transfer(SS,(unsigned char)c[i],SPI_CONTINUE);
   Serial.print(c[i]);
   //Serial.println(response);
-  delay(1000);
+  delay(10);
   }
 }
 
