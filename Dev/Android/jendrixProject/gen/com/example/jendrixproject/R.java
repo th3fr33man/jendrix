423b6751264d1fb81389e69238a03c8393769534
rom <raczkaromain@gmail.com>
2013-01-25 10:24:21 +0100

    maj jendrix project

--- Dev/Android/jendrixProject/gen/com/example/jendrixproject/R.java
+++ Dev/Android/jendrixProject/gen/com/example/jendrixproject/R.java
@@ -14,10 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int b1=0x7f070001;
-        public static final int b2=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int textView1=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int tb1=0x7f070000;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,6 +33,7 @@ public final class R {
         public static final int button=0x7f040003;
         public static final int editText=0x7f040004;
         public static final int hello_world=0x7f040001;
+        public static final int log=0x7f040007;
         public static final int menu_settings=0x7f040002;
     }
     public static final class style {
--- Dev/Android/jendrixProject/src/com/example/jendrixproject/MainActivity.java
+++ Dev/Android/jendrixProject/src/com/example/jendrixproject/MainActivity.java
@@ -1,50 +1,32 @@
 package com.example.jendrixproject;
-import android.view.View;
-//N'oubliez pas de déclarer le bon package dans lequel se trouve le fichier !
-
+import Model.Server;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-//	private TextView coucou = null;
-//	private String hello;
-private Button b1=null, b2=null;
-	
+
+private Button tb1=null;
+private Server ecoute;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
 		EditText editText = new EditText(this);
 		editText.setHint(R.string.editText);
 		editText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
 		editText.setLines(5);
 		
 		/* méthode à comprendre sert à La référence  des boutons */
-
 		setContentView(R.layout.activity_main);
-		
-	    b1=(Button)findViewById(R.id.b1);
-	    b2=(Button)findViewById(R.id.b2);
-		
-//		Button b1 = new Button(this);
-//		Button b2 = new Button(this);
+	    tb1=(Button)findViewById(R.id.tb1);
+		tb1.setOnClickListener(this);
 
-		b1.setOnClickListener(this);
-		b2.setOnClickListener(this);
-		
-
-		
-//		hello = getResources().getString(R.string.hello_world);
-//		hello = hello.replaceAll("world", "seicom");
-//		coucou = new TextView(this);
-//		coucou.setText(hello);
-//		setContentView(coucou);
 		
 	}
 	
@@ -54,22 +36,27 @@ private Button b1=null, b2=null;
 		  switch(v.getId()) {
 		 
 		    // Si l'identifiant de la vue est celui du premier bouton
-		    case R.id.b1:
+		    case R.id.tb1:
 		    /* Agir pour bouton 1 */
 		    Toast.makeText(this, "Lancement de l'écoute", Toast.LENGTH_SHORT).show();
+			ecoute = new Server();
+			ecoute.run();
 		    break;
 		 
+		    
 		    // Si l'identifiant de la vue est celui du deuxième bouton     
-		    case R.id.b2:
+//		    case R.id.tb2:
 		    /* Agir pour bouton 2 */
-		    Toast.makeText(this, "Lancement de l'enregistrement", Toast.LENGTH_LONG).show();
-		    break;
+//		    Toast.makeText(this, "Lancement de l'enregistrement", Toast.LENGTH_LONG).show();
+//		    break;
 		   
 		    /* etc. */
 		  }
 		}
 
 }
+
+
 
 /*
  * code par défaut lors de la création du projet
